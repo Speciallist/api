6ca9b0e4419a2c21265f5690f8a5602186cd921c
Speciallist <fhilype@hotmail.com>
2023-01-03 15:45:42 -0300

    Criação do projeto;

---
+++ src/test/java/io/github/Speciallist/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Speciallist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
