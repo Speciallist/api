e31fb760ac600cdd366767a021ae1e2613d29168
Speciallist <fhilype@hotmail.com>
2023-01-03 16:18:41 -0300

    Revert "Revert "Criação da classe User;""
    This reverts commit dc5514813ff7e5d966f423b2b71bb86a70773f45.

---
+++ src/main/java/io/github/Speciallist/api/domain/User.java
@@ -0,0 +1,36 @@
+package io.github.Speciallist.api.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+
+	private String password;
+
+}
