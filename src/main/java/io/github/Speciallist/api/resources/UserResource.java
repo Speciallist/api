71c2503296e5fa5377e8c940df73ed4dfefa6ab6
Speciallist <fhilype@hotmail.com>
2023-01-03 16:42:11 -0300

    Classe UserResource findById;

--- src/main/java/io/github/Speciallist/api/resources/UserResource.java
+++ src/main/java/io/github/Speciallist/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package io.github.Speciallist.api.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.Speciallist.api.domain.User;
+import io.github.Speciallist.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Integer id) {
-		return ResponseEntity.ok().body(new User(1, "Fhilype", "fhilype@email.com", "123"));
+		return ResponseEntity.ok().body(userService.findById(id));
 	}
 
 }
