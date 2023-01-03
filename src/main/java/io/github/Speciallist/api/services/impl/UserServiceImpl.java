9d6ed4c72b40c1b75c5dcb098d016f76104b478b
Speciallist <fhilype@hotmail.com>
2023-01-03 16:41:29 -0300

    Classe UserServiceImpl;

---
+++ src/main/java/io/github/Speciallist/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package io.github.Speciallist.api.services.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.Speciallist.api.domain.User;
+import io.github.Speciallist.api.repositories.UserRepository;
+import io.github.Speciallist.api.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findById(Integer id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
