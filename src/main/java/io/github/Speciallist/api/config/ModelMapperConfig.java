83061e63ad1f5da3a5cc663fa20d03558cb815f3
Speciallist <fhilype@hotmail.com>
2023-01-04 11:26:33 -0300

    Disponibilizando um model mapper em forma de Bean;

---
+++ src/main/java/io/github/Speciallist/api/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package io.github.Speciallist.api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+
+}
