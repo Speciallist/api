efad0d381e7700ecf31fc658fb8f085ab5a62c0f
Speciallist <fhilype@hotmail.com>
2023-01-05 13:55:14 -0300

    Classe de testes para UserResource iniciada;

---
+++ src/test/java/io/github/Speciallist/api/resources/UserResourceTest.java
@@ -0,0 +1,68 @@
+package io.github.Speciallist.api.resources;
+
+import io.github.Speciallist.api.domain.User;
+import io.github.Speciallist.api.domain.dto.UserDTO;
+import io.github.Speciallist.api.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final Integer ID = 4;
+    public static final String NAME = "Kodlak";
+    public static final String EMAIL = "kodlak@companions.com";
+    public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
+    public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource userResource;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
