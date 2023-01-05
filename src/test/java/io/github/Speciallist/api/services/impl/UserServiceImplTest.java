632cd5e1328fde19c0cdc102fb99a1fed7824d3a
Speciallist <fhilype@hotmail.com>
2023-01-05 11:02:29 -0300

    Teste para a classe UserService, método delete com sucesso;

--- src/test/java/io/github/Speciallist/api/services/impl/UserServiceImplTest.java
+++ src/test/java/io/github/Speciallist/api/services/impl/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -142,7 +142,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteWithSuccess() {
+        when(userRepository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(userRepository).deleteById(anyInt());
+        userService.delete(ID);
+        verify(userRepository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
